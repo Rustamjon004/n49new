bde203854ecfaa5f663c551a781f18ef63efef84
rustam <odiloffr@gmail.com>
2024-09-08 23:08:43 +0500

    dowloand admin page and new card

--- djangoProject/app_main/models.py
+++ djangoProject/app_main/models.py
@@ -1,3 +1,14 @@
+
 from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    title = models.CharField(max_length=100)
+    body = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.title} -{str(self.created_at)}"
 
 # Create your models here.
--- djangoProject/app_main/views.py
+++ djangoProject/app_main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 def home(request):
+    posts = Post.objects.all()
     return render(request=request,
-                  template_name='home.html')
+                  template_name='home.html',
+                context={'posts':posts})
 def dashboard(request):
     return render(request=request,
                   template_name='dashboard.html')
